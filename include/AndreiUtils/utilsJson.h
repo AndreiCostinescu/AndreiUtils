98b35b5f1bcdcb23e6896d273fa461431e7a3d45
Andrei Costinescu <andrei.costinescu@tum.de>
2021-10-05 16:13:08 +0200

    Json can now serialize also std::complex type

--- include/AndreiUtils/utilsJson.h
+++ include/AndreiUtils/utilsJson.h
@@ -7,11 +7,38 @@
 
 #include <AndreiUtils/json.hpp>
 #include <string>
+#include <complex>
 
 namespace AndreiUtils {
     nlohmann::json readJsonFile(const std::string &path);
 
     void writeJsonFile(const std::string &path, const nlohmann::json &content);
+}
+
+namespace nlohmann {
+    template<class T>
+    struct adl_serializer<std::complex<T>> {
+        static void to_json(nlohmann::json &j, const std::complex<T> &v) {
+            j["r"] = v.real();
+            j["i"] = v.imag();
+        }
+
+        static void from_json(const nlohmann::json &j, std::complex<T> &v) {
+            v.real(j["r"].get<T>());
+            v.imag(j["i"].get<T>());
+        }
+    };
+
+    template<typename T>
+    struct adl_serializer<std::vector<std::complex<T>>> {
+        static void to_json(nlohmann::json &j, const std::vector<std::complex<T>> &v) {
+            j = v;
+        }
+
+        static void from_json(const nlohmann::json &j, std::vector<std::complex<T>> &v) {
+            v = j.get<std::vector<std::complex<T>>>();
+        }
+    };
 }
 
 #endif //ANDREIUTILS_JSON_H
