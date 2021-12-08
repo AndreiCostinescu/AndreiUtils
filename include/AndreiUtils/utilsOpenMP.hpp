7d809ff36ddce60227c0c6703f2f7fcf93e497fd
Andrei Costinescu <andrei.costinescu@tum.de>
2021-12-08 13:48:35 +0100

    Compatibility issues with openmp on gcc versions fixed in utilsOpenMP.hpp

--- include/AndreiUtils/utilsOpenMP.hpp
+++ include/AndreiUtils/utilsOpenMP.hpp
@@ -11,7 +11,7 @@ namespace AndreiUtils {
     template<class T>
     void fastMemCopy(T *const dst, const T *const src, size_t size) {
         // memcpy(dst, src, sizeof(T) * size);
-        #pragma omp parallel for shared(size) default(none)
+        #pragma omp parallel for shared(size)
         for (size_t i = 0; i < size; i++) {
             dst[i] = src[i];
         }
