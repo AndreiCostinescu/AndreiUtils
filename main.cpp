97ce432443b98c2a2df58186eb485577ec5fb272
Andrei Costinescu <andrei.costinescu@tum.de>
2022-01-13 06:08:59 +0100

    Add utils time test in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,52 @@
 // Created by andrei on 05.10.21.
 //
 
-#include <AndreiUtils/utilsJsonEigen.hpp>
 #include <AndreiUtils/utilsEigenOpenCV.h>
+#include <AndreiUtils/utilsJsonEigen.hpp>
+#include <AndreiUtils/utilsTime.h>
 #include <iostream>
 
 using namespace AndreiUtils;
 using namespace Eigen;
 using namespace std;
+
+void timeTesting() {
+    auto now = std::chrono::system_clock::now();
+    string time;
+    time = AndreiUtils::convertChronoToStringWithSubseconds(now, "%Y-%m-%d-%H-%M-%S", "%us-%pns", ":");
+    cout << "Initial time = " << time << endl;
+    time = AndreiUtils::convertChronoToStringWithSubseconds(now, "%Y-%m-%d-%H-%M-%S", "%ns", ":");
+    cout << "Initial time = " << time << endl;
+    time = AndreiUtils::convertChronoToStringWithSubseconds(now, "%Y-%m-%d-%H-%M-%S", "%ms-%pus-%pns", ":");
+    cout << "Initial time = " << time << endl;
+
+    auto x = AndreiUtils::convertStringToChronoWithSubseconds(time, "%Y-%m-%d-%H-%M-%S", "%ms-%pus-%pns", ":");
+
+    auto d = x.time_since_epoch();
+    /* UTC: +1:00 */
+    using Days = std::chrono::duration<int, std::ratio_multiply<std::chrono::hours::period, std::ratio<24>>::type>;
+    Days days = std::chrono::duration_cast<Days>(d);
+    d -= days;
+    auto hours = std::chrono::duration_cast<std::chrono::hours>(d);
+    d -= hours;
+    auto minutes = std::chrono::duration_cast<std::chrono::minutes>(d);
+    d -= minutes;
+    auto seconds = std::chrono::duration_cast<std::chrono::seconds>(d);
+    d -= seconds;
+    auto milliseconds = std::chrono::duration_cast<std::chrono::milliseconds>(d);
+    d -= milliseconds;
+    auto microseconds = std::chrono::duration_cast<std::chrono::microseconds>(d);
+    d -= microseconds;
+    auto nanoseconds = std::chrono::duration_cast<std::chrono::nanoseconds>(d);
+    cout << hours.count() << ":" << minutes.count() << ":" << seconds.count() << ":" << milliseconds.count() << ":"
+         << microseconds.count() << ":" << nanoseconds.count() << endl;
+
+    cout << AndreiUtils::convertChronoToStringWithSubseconds(x, "%Y-%m-%d-%H-%M-%S", "%ns", ":") << endl;
+    cout << AndreiUtils::convertChronoToStringWithSubseconds(x, "%Y-%m-%d-%H-%M-%S", "%pns", ":") << endl;
+    cout << AndreiUtils::convertChronoToStringWithSubseconds(x, "%Y-%m-%d-%H-%M-%S", "%us", ":") << endl;
+    cout << AndreiUtils::convertChronoToStringWithSubseconds(x, "%Y-%m-%d-%H-%M-%S", "%pus", ":") << endl;
+    cout << AndreiUtils::convertChronoToStringWithSubseconds(x, "%Y-%m-%d-%H-%M-%S", "%ms", ":") << endl;
+}
 
 int main() {
     cout << "Hello World!" << endl;
