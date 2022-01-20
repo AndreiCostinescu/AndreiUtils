1b1c04b85f1c97f340aa7b1872fd2f207f99bdcf
Andrei Costinescu <andrei.costinescu@yahoo.com>
2022-01-20 14:15:18 +0100

    Add new utility functions in utilsOpenMP.hpp to operate elementwise on source and put result in dst

--- include/AndreiUtils/utilsOpenMP.hpp
+++ include/AndreiUtils/utilsOpenMP.hpp
@@ -5,6 +5,7 @@
 #ifndef ANDREIUTILS_UTILSOPENMP_HPP
 #define ANDREIUTILS_UTILSOPENMP_HPP
 
+#include <functional>
 #include <omp.h>
 
 namespace AndreiUtils {
@@ -14,6 +15,30 @@ namespace AndreiUtils {
         #pragma omp parallel for shared(size)
         for (size_t i = 0; i < size; i++) {
             dst[i] = src[i];
+        }
+    }
+
+    template<class T>
+    void fastATimesSrcPlusB(T *const dst, const T *const src, size_t size, T a, T b) {
+        #pragma omp parallel for shared(size)
+        for (size_t i = 0; i < size; i++) {
+            dst[i] = a * src[i] + b;
+        }
+    }
+
+    template<class T>
+    void fastSrcOp(T *const dst, const T *const src, size_t size, std::function<T(const T &)> op) {
+        #pragma omp parallel for shared(size)
+        for (size_t i = 0; i < size; i++) {
+            dst[i] = op(src[i]);
+        }
+    }
+
+    template<class Tin, class Tout>
+    void fastSrcOp(Tout *const dst, const Tin *const src, size_t size, std::function<Tout(const Tin &)> op) {
+        #pragma omp parallel for shared(size)
+        for (size_t i = 0; i < size; i++) {
+            dst[i] = op(src[i]);
         }
     }
 }
