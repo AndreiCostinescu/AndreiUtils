d975fd85bfd1a3042dc6e929c6aa70abb6d6bea0
Andrei Costinescu <andreicostinescu96@gmail.com>
2022-12-03 14:16:40 +0100

    Add option to the MixedDataContainer to not pass pointers to data, but actual objects that will be created, deleted and managed by this class + add test

--- include/AndreiUtils/classes/MixedDataContainer.hpp
+++ include/AndreiUtils/classes/MixedDataContainer.hpp
@@ -8,6 +8,7 @@
 #include <AndreiUtils/utilsMap.hpp>
 #include <map>
 #include <string>
+#include <type_traits>
 #include <utility>
 
 namespace AndreiUtils {
@@ -15,23 +16,76 @@ namespace AndreiUtils {
     public:
         MixedDataContainer();
 
-        virtual ~MixedDataContainer();
+        MixedDataContainer(MixedDataContainer const &other) = delete;
 
+        MixedDataContainer(MixedDataContainer &&other) noexcept;
+
+        MixedDataContainer &operator=(MixedDataContainer const &other) = delete;
+
+        MixedDataContainer &operator=(MixedDataContainer &&other) noexcept;
+
+        virtual ~MixedDataContainer();
+\
         template<typename T>
-        void addData(const std::string &id, T *x, bool errorOnReplace) {
-            if (mapContains(this->data, id) && errorOnReplace) {
-                throw std::runtime_error("Data " + id + " is already in the container! Not replacing!");
-            }
+        void addData(std::string const &id, T *x, bool errorOnReplace) {
+            this->checkErrorOnReplace(id, errorOnReplace);
             this->addData(id, x);
         }
 
         template<typename T>
-        void addData(const std::string &id, T *x) {
+        void addData(std::string const &id, T *x) {
             mapSet(this->data, id, (void *) x);
         }
 
         template<typename T>
-        T *getData(const std::string &id) const {
+        void addData(std::string const &id, T const &x, bool errorOnReplace) {
+            this->checkErrorOnReplace(id, errorOnReplace);
+            this->addData(id, x);
+        }
+
+        template<typename T>
+        void addData(std::string const &id, T const &x) {
+            this->template addData(id, x, this->template defaultDeleter<T>());
+        }
+
+        template<typename T>
+        void addData(std::string const &id, T const &x, std::function<void(void *)> const &deleter,
+                     bool errorOnReplace) {
+            this->checkErrorOnReplace(id, errorOnReplace);
+            this->addData(id, x, deleter);
+        }
+
+        template<typename T>
+        void addData(std::string const &id, T const &x, std::function<void(void *)> const &deleter) {
+            mapSet(this->data, id, (void *) new T(x));
+            mapSet(this->deleters, id, deleter);
+        }
+
+        template<typename T>
+        void addData(std::string const &id, T &&x, bool errorOnReplace) {
+            this->checkErrorOnReplace(id, errorOnReplace);
+            this->addData(id, std::forward<T>(x));
+        }
+
+        template<typename T>
+        void addData(std::string const &id, T &&x) {
+            this->template addData(id, std::forward<T>(x), this->template defaultDeleter<T>());
+        }
+
+        template<typename T>
+        void addData(std::string const &id, T &&x, std::function<void(void *)> const &deleter, bool errorOnReplace) {
+            this->checkErrorOnReplace(id, errorOnReplace);
+            this->addData(id, std::forward<T>(x), deleter);
+        }
+
+        template<typename T>
+        void addData(std::string const &id, T &&x, std::function<void(void *)> const &deleter) {
+            mapSet(this->data, id, (void *) new typename std::remove_reference<T>::type(std::forward<T>(x)));
+            mapSet(this->deleters, id, deleter);
+        }
+
+        template<typename T>
+        T *getData(std::string const &id) const {
             void *res = nullptr;
             if (!mapGetIfContains<std::string, void *>(this->data, id, res)) {
                 throw std::runtime_error("Data " + id + " not registered in container!");
@@ -40,7 +94,18 @@ namespace AndreiUtils {
         }
 
     protected:
+        void checkErrorOnReplace(std::string const &id, bool errorOnReplace) const;
+
+        template<typename T>
+        std::function<void(void *)> defaultDeleter() {
+            return [](void *_data) {
+                std::cout << "Enter default deleter " << std::endl;
+                delete (typename std::remove_reference<T>::type *) _data;
+            };
+        }
+
         std::map<std::string, void *> data;
+        std::map<std::string, std::function<void(void *)>> deleters;
     };
 }
 
--- src/classes/MixedDataContainer.cpp
+++ src/classes/MixedDataContainer.cpp
@@ -3,10 +3,35 @@
 //
 
 #include <AndreiUtils/classes/MixedDataContainer.hpp>
+#include <AndreiUtils/utilsMap.hpp>
 
 using namespace AndreiUtils;
 using namespace std;
 
 MixedDataContainer::MixedDataContainer() : data() {}
 
-MixedDataContainer::~MixedDataContainer() = default;
+MixedDataContainer::MixedDataContainer(MixedDataContainer &&other) noexcept:
+        data(std::move(other.data)), deleters(std::move(other.deleters)) {}
+
+MixedDataContainer &MixedDataContainer::operator=(MixedDataContainer &&other) noexcept {
+    if (this != &other) {
+        this->data = std::move(other.data);
+        this->deleters = std::move(other.deleters);
+    }
+    return *this;
+}
+
+MixedDataContainer::~MixedDataContainer() {
+    std::function<void(void *)> deleter;
+    for (auto const &name: this->data) {
+        if (mapGetIfContains(this->deleters, name.first, deleter)) {
+            deleter(name.second);
+        }
+    }
+}
+
+void MixedDataContainer::checkErrorOnReplace(string const &id, bool errorOnReplace) const {
+    if (errorOnReplace && mapContains(this->data, id)) {
+        throw std::runtime_error("Data " + id + " is already in the container! Not replacing!");
+    }
+}
