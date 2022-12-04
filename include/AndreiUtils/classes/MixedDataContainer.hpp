810e57829549aa1e6445aec82c77aec6e8a5a944
Andrei Costinescu <andreicostinescu96@gmail.com>
2022-12-04 09:35:32 +0100

    Handle overwriting data in the MixedDataContainer (that data must be deleted if overwritten!)

--- include/AndreiUtils/classes/MixedDataContainer.hpp
+++ include/AndreiUtils/classes/MixedDataContainer.hpp
@@ -25,7 +25,9 @@ namespace AndreiUtils {
         MixedDataContainer &operator=(MixedDataContainer &&other) noexcept;
 
         virtual ~MixedDataContainer();
-\
+
+        bool has(std::string const &id) const;
+
         template<typename T>
         void addData(std::string const &id, T *x, bool errorOnReplace) {
             this->checkErrorOnReplace(id, errorOnReplace);
@@ -34,7 +36,33 @@ namespace AndreiUtils {
 
         template<typename T>
         void addData(std::string const &id, T *x) {
+            this->removeData(id);
             mapSet(this->data, id, (void *) x);
+        }
+
+        template<typename T>
+        void addDataPassPointerOwnership(std::string const &id, T *x, bool errorOnReplace) {
+            this->checkErrorOnReplace(id, errorOnReplace);
+            this->template addDataPassPointerOwnership(id, x);
+        }
+
+        template<typename T>
+        void addDataPassPointerOwnership(std::string const &id, T *x) {
+            this->template addDataPassPointerOwnership(id, x, this->template defaultDeleter<T>());
+        }
+
+        template<typename T>
+        void addDataPassPointerOwnership(std::string const &id, T *x, std::function<void(void *)> const &deleter,
+                                         bool errorOnReplace) {
+            this->checkErrorOnReplace(id, errorOnReplace);
+            this->template addDataPassPointerOwnership(id, x, deleter);
+        }
+
+        template<typename T>
+        void addDataPassPointerOwnership(std::string const &id, T *x, std::function<void(void *)> const &deleter) {
+            this->removeData(id);
+            mapSet(this->data, id, (void *) x);
+            mapSet(this->deleters, id, deleter);
         }
 
         template<typename T>
@@ -57,6 +85,7 @@ namespace AndreiUtils {
 
         template<typename T>
         void addData(std::string const &id, T const &x, std::function<void(void *)> const &deleter) {
+            this->removeData(id);
             mapSet(this->data, id, (void *) new T(x));
             mapSet(this->deleters, id, deleter);
         }
@@ -80,20 +109,60 @@ namespace AndreiUtils {
 
         template<typename T>
         void addData(std::string const &id, T &&x, std::function<void(void *)> const &deleter) {
+            this->removeData(id);
             mapSet(this->data, id, (void *) new typename std::remove_reference<T>::type(std::forward<T>(x)));
             mapSet(this->deleters, id, deleter);
         }
 
+        void *getData(std::string const &id) const;
+
         template<typename T>
         T *getData(std::string const &id) const {
-            void *res = nullptr;
-            if (!mapGetIfContains<std::string, void *>(this->data, id, res)) {
-                throw std::runtime_error("Data " + id + " not registered in container!");
-            }
-            return (T *) res;
+            return (T *) this->getData(id);
         }
 
+        bool getDataIfContains(std::string const &id, void *&res) const;
+
+        template<typename T>
+        bool getDataIfContains(std::string const &id, T *&res) const {
+            void *_res = nullptr;
+            if (this->getDataIfContains(id, _res)) {
+                res = (T *) _res;
+                return true;
+            }
+            return false;
+        }
+
+        template<typename T>
+        void updateData(std::string const &id, T const &x) {
+            void *_data;
+            if (!AndreiUtils::mapGetIfContains(this->data, id, _data)) {
+                throw std::runtime_error("Can not update data in container if no data is present (id = " + id + ")");
+            }
+            *(T *) _data = x;
+        }
+
+        std::map<std::string, void *>::iterator begin();
+
+        std::map<std::string, void *>::iterator end();
+
+        std::map<std::string, void *>::const_iterator begin() const;
+
+        std::map<std::string, void *>::const_iterator end() const;
+
     protected:
+        void removeData(std::string const &id) {
+            void *datum;
+            if (AndreiUtils::mapGetIfContains(this->data, id, datum)) {
+                std::function<void(void *)> *deleter;
+                if (AndreiUtils::mapGetIfContains(this->deleters, id, deleter)) {
+                    (*deleter)(datum);
+                    AndreiUtils::mapDelete(this->deleters, id);
+                }
+                AndreiUtils::mapDelete(this->data, id);
+            }
+        }
+
         void checkErrorOnReplace(std::string const &id, bool errorOnReplace) const;
 
         template<typename T>
--- src/classes/MixedDataContainer.cpp
+++ src/classes/MixedDataContainer.cpp
@@ -30,6 +30,38 @@ MixedDataContainer::~MixedDataContainer() {
     }
 }
 
+bool MixedDataContainer::has(string const &id) const {
+    return AndreiUtils::mapContains(this->data, id);
+}
+
+void *MixedDataContainer::getData(string const &id) const {
+    void *res = nullptr;
+    if (!mapGetIfContains<std::string, void *>(this->data, id, res)) {
+        throw std::runtime_error("Data " + id + " not registered in container!");
+    }
+    return res;
+}
+
+bool MixedDataContainer::getDataIfContains(string const &id, void *&res) const {
+    return mapGetIfContains<std::string, void *>(this->data, id, res);
+}
+
+std::map<std::string, void *>::iterator MixedDataContainer::begin() {
+    return this->data.begin();
+}
+
+std::map<std::string, void *>::iterator MixedDataContainer::end() {
+    return this->data.end();
+}
+
+std::map<std::string, void *>::const_iterator MixedDataContainer::begin() const {
+    return this->data.begin();
+}
+
+std::map<std::string, void *>::const_iterator MixedDataContainer::end() const {
+    return this->data.end();
+}
+
 void MixedDataContainer::checkErrorOnReplace(string const &id, bool errorOnReplace) const {
     if (errorOnReplace && mapContains(this->data, id)) {
         throw std::runtime_error("Data " + id + " is already in the container! Not replacing!");
