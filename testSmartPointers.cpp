8b151f7fc4a4c819a0016248a661f7d6e2c45b64
Andrei Costinescu <andreicostinescu96@gmail.com>
2022-12-13 09:25:38 +0100

    Add more smart pointer tests

--- testSmartPointers.cpp
+++ testSmartPointers.cpp
@@ -9,7 +9,7 @@ using namespace std;
 
 class A {
 public:
-    explicit A(string name) : name(std::move(name)) {}
+    explicit A(string name = "") : name(std::move(name)) {}
 
     ~A() {
         cout << "In A's destructor for " << this->name << "!" << endl;
@@ -18,13 +18,11 @@ public:
     string name;
 };
 
-void print(A *a) {
+void print(A const *a) {
     cout << a->name << endl;
 }
 
-int main() {
-    cout << "Hello World!" << endl;
-
+void testFirstExample() {
     std::unique_ptr<A>(new A("1"));
     cout << "After temporary object..." << endl;
     std::unique_ptr<A> a(new A("2"));
@@ -35,7 +33,9 @@ int main() {
     print(make_shared<A>("213").get());
     A b("4");
     print(&b);
+}
 
+void testResetPointer() {
     auto c = make_shared<A>("1234");
     cout << c.use_count() << endl;
     auto d = c;
@@ -45,6 +45,75 @@ int main() {
     d.reset();
     cout << c.use_count() << endl;
     cout << d.use_count() << endl;
+}
+
+void testWeakPtr() {
+    cout << endl << endl << "Test Weak Ptr:" << endl;
+    // initial state
+    auto teacher = make_shared<A>("Jack");
+    auto coach = teacher;
+    weak_ptr<A> wp = teacher;
+
+    cout << coach.use_count() << endl;
+    if (auto temp = wp.lock()) {
+        cout << coach.use_count() << endl;
+        cout << temp->name << endl;
+    }
+    cout << coach.use_count() << endl;
+
+    // coach is reset
+    coach.reset();
+
+    if (!wp.expired()) {
+        cout << "The old teacher is still there." << endl;
+    }
+
+    // teacher is reset to Rose
+    teacher.reset(new A("Rose"));
+
+    if (wp.expired()) {
+        cout << "The old teacher is not there." << endl;
+    }
+    if (!wp.lock()) {
+        cout << "The old teacher is still not there." << endl;
+    }
+    if (wp.lock() == nullptr) {
+        cout << "The old teacher is still not there." << endl;
+    }
+}
+
+void testSharedPtrWithConst() {
+    shared_ptr<A const> a = make_shared<A>("123");
+    shared_ptr<A const> c = a;
+    print(c.get());
+    shared_ptr<A> b = make_shared<A>("321");
+    c = b;
+    print(c.get());
+}
+
+void testSharedPtrWithArray() {
+    int n = 10;
+    auto x = shared_ptr<A[]>(new A[n]);
+    for (int i = 0; i < n; i++) {
+        x[i].name = to_string(i);
+    }
+    x.reset();
+}
+
+void testVoidSharedPtr() {
+    std::shared_ptr<void> x = make_shared<A>("12345");
+    x.reset();
+}
+
+int main() {
+    cout << "Hello World!" << endl;
+
+    // testFirstExample();
+    // testResetPointer();
+    // testWeakPtr();
+    // testSharedPtrWithConst();
+    // testSharedPtrWithArray();
+    testVoidSharedPtr();
 
     return 0;
 }
