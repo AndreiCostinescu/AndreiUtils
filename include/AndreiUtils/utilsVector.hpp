95884b07c00df5f90c50e904a1b45b56f63a0f36
Andrei Costinescu <andrei.costinescu@tum.de>
2023-09-28 11:05:03 +0200

    Reformat some utilsVector functions

--- include/AndreiUtils/utilsVector.hpp
+++ include/AndreiUtils/utilsVector.hpp
@@ -551,16 +551,16 @@ namespace AndreiUtils {
     }
 
     template<class T>
-    [[nodiscard]] std::vector<T>
-    vectorAppend(std::vector<T> const &container, std::vector<T> const &valuesToBeAppended) {
+    [[nodiscard]] std::vector<T> vectorAppend(
+            std::vector<T> const &container, std::vector<T> const &valuesToBeAppended) {
         auto res = container;
         res.insert(res.end(), valuesToBeAppended.begin(), valuesToBeAppended.end());
         return res;
     }
 
     template<class T>
-    [[nodiscard]] std::vector<T>
-    vectorAppend(std::vector<T> const &container, T const *valuesToBeAppended, size_t const &size) {
+    [[nodiscard]] std::vector<T> vectorAppend(
+            std::vector<T> const &container, T const *valuesToBeAppended, size_t const &size) {
         std::vector<T> res = container;
         size_t containerSize = container.size();
         res.resize(containerSize + size);
