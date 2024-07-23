a30cd3f3d653595a5a50b21bd2c029ae780b109f
Andrei Costinescu <andrei.costinescu@tum.de>
2024-07-23 10:34:52 +0200

    Revert: Add alias for the template type in stringify.hpp

--- include/AndreiUtils/traits/stringify.hpp
+++ include/AndreiUtils/traits/stringify.hpp
@@ -15,8 +15,6 @@
 namespace AndreiUtils {
     template<typename T>
     struct stringify {
-        using Type = T;
-
         static std::string to_string(T const &datum) {
             std::stringstream ss;
             ss << datum;
