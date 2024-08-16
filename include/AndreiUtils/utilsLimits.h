8f4e1ad6151e502f873d0d9c536d179c4a64e3be
Andrei Costinescu <andrei.costinescu@yahoo.com>
2024-08-16 07:57:41 +0200

    Add my own max double limit (which is half of the maximally representable double value) to have the interval (-inf, inf) representable and sampleable (the size of the interval would otherwise be 2 * double_max... which is not representable :D)

---
+++ include/AndreiUtils/utilsLimits.h
@@ -0,0 +1,12 @@
+//
+// Created by Andrei on 16.08.24.
+//
+
+#pragma once
+
+#include <limits>
+
+namespace AndreiUtils {
+    extern double const DOUBLE_INF;
+    extern double const DOUBLE_INF_NEG;
+}
---
+++ src/utilsLimits.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Andrei on 16.08.24.
+//
+
+#include <AndreiUtils/utilsLimits.h>
+
+double const AndreiUtils::DOUBLE_INF = std::numeric_limits<double>::max() / 2;
+double const AndreiUtils::DOUBLE_INF_NEG = -DOUBLE_INF;
