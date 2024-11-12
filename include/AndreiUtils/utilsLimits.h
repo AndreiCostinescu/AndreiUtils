e77ea1663c5ae161280d0c8cc2e99017a42a0320
Andrei Costinescu <andrei.costinescu@tum.de>
2024-11-12 14:15:00 +0100

    Add LONG_INF definitions in utilsLimits

--- include/AndreiUtils/utilsLimits.h
+++ include/AndreiUtils/utilsLimits.h
@@ -9,4 +9,6 @@
 namespace AndreiUtils {
     extern double const DOUBLE_INF;
     extern double const DOUBLE_INF_NEG;
+    extern long const LONG_INF;
+    extern long const LONG_INF_NEG;
 }
--- src/utilsLimits.cpp
+++ src/utilsLimits.cpp
@@ -6,3 +6,5 @@
 
 double const AndreiUtils::DOUBLE_INF = std::numeric_limits<double>::max() / 2;
 double const AndreiUtils::DOUBLE_INF_NEG = -DOUBLE_INF;
+long const AndreiUtils::LONG_INF = std::numeric_limits<long>::max() / 2 + 1;  // 31 / 2 = 15; + 1 = 16;
+long const AndreiUtils::LONG_INF_NEG = std::numeric_limits<long>::min() / 2;
