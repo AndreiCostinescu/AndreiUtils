a98e3b5e7893b0017921136b08f2b1fedc225593
Andrei Costinescu <andrei.costinescu@yahoo.com>
2026-03-15 08:20:16 +0100

    Make explicit external namespaces in Grasp and Symmetry implementation

--- src/classes/Symmetry.cpp
+++ src/classes/Symmetry.cpp
@@ -6,8 +6,6 @@
 #include <AndreiUtils/classes/RandomNumberGenerator.hpp>
 
 using namespace AndreiUtils;
-using namespace Eigen;
-using namespace std;
 
 Symmetry::Symmetry() = default;
 
@@ -21,7 +19,8 @@ Posed Symmetry::getSymmetricTransformation(double rangeValue) const {
     Posed randomPose;
     if (this->type == "rotation") {
         // TODO: what about the axisDisplacement?!
-        randomPose = Posed(Quaterniond(AngleAxis<double>(rangeValue, this->axis)), Vector3d::Zero());
+        randomPose = Posed(Eigen::Quaterniond(Eigen::AngleAxis<double>(rangeValue, this->axis)),
+                           Eigen::Vector3d::Zero());
     } else {
         // TODO: what about the axisDisplacement?!
         randomPose = Posed(qIdentity<double>(), rangeValue * this->axis);
@@ -34,8 +33,8 @@ Posed Symmetry::getSymmetricTransformation() const {
     return this->getSymmetricTransformation(randomRange);
 }
 
-vector<Posed> Symmetry::createSymmetricPoses(vector<Posed> const &poses, double const &rangeValue) const {
-    vector<Posed> newPoses;
+std::vector<Posed> Symmetry::createSymmetricPoses(std::vector<Posed> const &poses, double const &rangeValue) const {
+    std::vector<Posed> newPoses;
     auto randomPose = this->getSymmetricTransformation(rangeValue);
     for (auto const &pose: poses) {
         newPoses.push_back(randomPose * pose);
@@ -43,8 +42,8 @@ vector<Posed> Symmetry::createSymmetricPoses(vector<Posed> const &poses, double 
     return newPoses;
 }
 
-vector<Posed> Symmetry::createSymmetricPoses(vector<Posed> const &poses) const {
-    vector<Posed> newPoses;
+std::vector<Posed> Symmetry::createSymmetricPoses(std::vector<Posed> const &poses) const {
+    std::vector<Posed> newPoses;
     auto randomPose = this->getSymmetricTransformation();
     for (auto const &pose: poses) {
         newPoses.push_back(randomPose * pose);
@@ -60,13 +59,13 @@ Posed Symmetry::createSymmetricPose(Posed const &pose) const {
     return this->getSymmetricTransformation() * pose;
 }
 
-bool Symmetry::checkIfOrientationFitsSymmetry(Quaterniond const &q, double axisSimilarityThreshold) const {
-    AngleAxisd qAxisAngle(q);
+bool Symmetry::checkIfOrientationFitsSymmetry(Eigen::Quaterniond const &q, double axisSimilarityThreshold) const {
+    Eigen::AngleAxisd qAxisAngle(q);
     double &qAngle = qAxisAngle.angle();  // <- this is always in [0, pi]
     if (AndreiUtils::equal<double>(qAngle, 0)) {
         return true;
     }
-    Vector3d &qAxis = qAxisAngle.axis();
+    Eigen::Vector3d &qAxis = qAxisAngle.axis();
 
     bool posCheck = (qAxis - this->axis).norm() <= axisSimilarityThreshold;
     bool negCheck = (qAxis + this->axis).norm() <= axisSimilarityThreshold;
--- src/classes/grasp/Grasp.cpp
+++ src/classes/grasp/Grasp.cpp
@@ -6,12 +6,11 @@
 #include <AndreiUtils/utilsGeometry.h>
 
 using namespace AndreiUtils;
-using namespace std;
 
-Grasp::Grasp(vector<Posed> const &graspPointPoses, bool hasGraspAngleRange, pair<double, double> const &graspAngleRange)
-    : graspPointPoses(graspPointPoses), originalGraspPointPoses(graspPointPoses), graspAngleRange(graspAngleRange),
-      hasGraspAngleRange(hasGraspAngleRange) {
-}
+Grasp::Grasp(std::vector<Posed> const &graspPointPoses, bool hasGraspAngleRange,
+             std::pair<double, double> const &graspAngleRange) :
+        graspPointPoses(graspPointPoses), originalGraspPointPoses(graspPointPoses), graspAngleRange(graspAngleRange),
+        hasGraspAngleRange(hasGraspAngleRange) {}
 
 Grasp::~Grasp() = default;
 
@@ -26,12 +25,12 @@ size_t Grasp::getNrPoints() const {
 
 void Grasp::update() {}
 
-vector<Posed> const &Grasp::getCurrentGraspPointPoses() {
+std::vector<Posed> const &Grasp::getCurrentGraspPointPoses() {
     this->update();
     return this->graspPointPoses;
 }
 
-vector<Posed> const &Grasp::getGraspPointPoses() const {
+std::vector<Posed> const &Grasp::getGraspPointPoses() const {
     return this->graspPointPoses;
 }
 
@@ -45,11 +44,11 @@ void Grasp::composeRelativeTo(AndreiUtils::Posed const &graspRelativeTo) {
     this->updateGraspPointPosesFromRelativeTo();
 }
 
-pair<double, double> const &Grasp::getGraspAngleRange() const {
+std::pair<double, double> const &Grasp::getGraspAngleRange() const {
     return this->graspAngleRange;
 }
 
-pair<double, double> &Grasp::getGraspAngleRange() {
+std::pair<double, double> &Grasp::getGraspAngleRange() {
     return this->graspAngleRange;
 }
 
